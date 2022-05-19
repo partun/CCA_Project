086fcc19a3711c85931a3c0098c3c304f14397be
Dominic <16167645+partun@users.noreply.github.com>
2022-05-19 16:02:32 +0200

    plots for part3a and part4.1a/d

---
+++ part3/plots.py
@@ -0,0 +1,118 @@
+from cProfile import label, run
+from itertools import chain
+import pandas as pd
+from datetime import datetime, timedelta, time
+import matplotlib.pyplot as plt
+
+
+def create_all_plots_for_3a():
+    for i in range(3):
+        plot3a(i + 1)
+
+
+def plot3a(run_index: int):
+
+    with open(f'schedules/memcached_log_03_run{run_index:d}.csv') as log_file:
+        memcached_qos = pd.read_csv(log_file, delim_whitespace=True, header=1)
+
+    with open(f'schedules/memcached_log_03_run{run_index:d}.csv') as log_file:
+        for line in log_file.readlines():
+            start_time = datetime.strptime(
+                line[:-1], r"%a %b  %d %H:%M:%S %Z %Y")
+            break
+
+    interval = timedelta(seconds=20)
+    memcached_qos['timestamp'] = pd.Series((
+        (i + 1) * 20 for i in range(memcached_qos.shape[0])
+    ))
+
+    def convert_to_relative_time(time_str):
+        return (
+            datetime.strptime(time_str, r"%H:%M:%S").replace(
+                year=start_time.year, month=start_time.month, day=start_time.day
+            )
+            - start_time
+        ).total_seconds()
+
+    with open(f'schedules/times_{run_index:d}.csv') as times_file:
+        workload_times = pd.read_csv(times_file, sep=';')
+        workload_times['start'] = workload_times['start'].map(
+            convert_to_relative_time)
+        workload_times['end'] = workload_times['end'].map(
+            convert_to_relative_time)
+
+    print(workload_times)
+
+    # Setup figure
+    fig = plt.figure(figsize=(10, 5))
+    fig_ax = fig.gca()
+
+    # Plot line
+    fig_ax.plot(
+        memcached_qos['timestamp'],
+        memcached_qos['p95'] / 1000,
+        label='p95',
+        marker="o",
+        markersize=8,
+        markerfacecolor="none",
+    )
+
+    colors = ('blue', 'orange', 'green', 'red', 'purple', 'pink')
+    for i, (_, workload) in enumerate(sorted(workload_times.iterrows(), key=lambda x: x[1]['start'])):
+        # fig_ax.plot(
+        #     [workload['start'], workload['start']],
+        #     [0, 1000],
+        #     label=workload['workload'],
+        #     markersize=8,
+        #     markerfacecolor="none",
+        # )
+
+        fig_ax.broken_barh(
+            [(workload['start'], workload['end'])],
+            (0.1 * i, 0.1),
+            label=workload['workload'],
+            facecolors=colors[i]
+        )
+        pass
+
+    # workloads = sorted(workload_times.iterrows(),
+    #                    key=lambda x: x[1]['end'], reverse=True)
+
+    # labels = (w['workload'] for i, w in workloads)
+    # xrow = sorted(
+    #     (x for x in chain(workload_times['start'], workload_times['end'])))
+    # y = [
+    #     [
+    #         100 if workload_times['start'][i] <= x <= workload_times['end'][i] else 0
+    #         for x in xrow
+    #     ]
+    #     for i, workload in workloads
+    # ]
+
+    # fig_ax.stackplot(xrow, y, labels=labels)
+
+    # # Style figure
+    # fig_ax.set_title(
+    #     "Response time of System A.\nError bars: $1s$. (10 rep. per point)",
+    #     fontsize=16,
+    #     fontweight="bold",
+    #     pad=10
+    # )
+    fig_ax.set_xlabel("Time", fontsize=16)
+    fig_ax.set_ylabel("95th %-tile response time (ms)", fontsize=16)
+    fig_ax.legend(loc='upper right', fontsize=14)
+    fig_ax.grid(True, color='lightgray', linestyle='--', linewidth=1)
+    # fig_ax.tick_params(labelsize=12)
+    fig_ax.set_xlim(left=0, right=350)
+    fig_ax.set_ylim(bottom=0, top=0.6)
+    # fig_ax.set_yticks(range(0, 11, 1))
+    # fig_ax.set_xticks(range(0, 80001, 10000), labelrotation=45)
+    # fig_ax.set_xticks(range(0, 80001, 5000), minor=True)
+
+    # Save plot
+    plt.tight_layout()
+    plt.savefig(f"plot3a_run{run_index:d}.pdf")
+
+
+if __name__ == '__main__':
+    create_all_plots_for_3a()
---
+++ part4/4.1/plots41.py
@@ -0,0 +1,173 @@
+from cProfile import run
+from itertools import product
+import pandas as pd
+import matplotlib.pyplot as plt
+from psutil import cpu_percent, cpu_stats
+
+
+def plot41a(run_cnt=3):
+    # Setup figure
+    fig = plt.figure(figsize=(10, 5))
+    fig_ax = fig.gca()
+
+    for t, c in product((1, 2), (1, 2)):
+        qps = []
+        for i in range(run_cnt):
+            with open(f'c{c}_t{t}/memcached{i+1}_c{c}_t{t}', 'r') as input_file:
+                qps.append(pd.read_csv(
+                    input_file, delim_whitespace=True, header=1))
+
+        qps_traces = pd.DataFrame(
+            {f'run{i}': qps[i]['QPS'] for i in range(run_cnt)})
+
+        p95_traces = pd.DataFrame(
+            {f'run{i}': qps[i]['p95'] / 1000 for i in range(run_cnt)})
+
+        # print(qps_traces)
+
+        fig_ax.errorbar(
+            x=qps_traces.mean(axis=1),
+            y=p95_traces.mean(axis=1),
+            xerr=qps_traces.std(axis=1),
+            yerr=p95_traces.std(axis=1),
+            label=f'T={t}, C={c}',
+            marker='o' if c == 1 else 'x',
+            linestyle='-' if t == 1 else '--',
+            markersize=8,
+            markerfacecolor="none",
+            capsize=4
+        )
+
+    fig_ax.plot(
+        [0, 125000],
+        [1.5, 1.5],
+        linestyle=':',
+        label='SLO'
+    )
+
+    fig_ax.set_xlabel("QPS", fontsize=16)
+    fig_ax.set_ylabel("95th %-tile response time (ms)", fontsize=16)
+    fig_ax.legend(loc='upper left', fontsize=14)
+    fig_ax.grid(True, color='lightgray', linestyle='--', linewidth=1)
+    fig_ax.tick_params(labelsize=12)
+    fig_ax.set_xlim(left=0, right=125000)
+    fig_ax.set_ylim(bottom=0, top=2)
+    # fig_ax.set_yticks(range(0, 11, 1))
+    # fig_ax.set_xticks(range(0, 80001, 10000), labelrotation=45)
+    # fig_ax.set_xticks(range(0, 80001, 5000), minor=True)
+
+    # Save plot
+    plt.tight_layout()
+    plt.savefig("plot41a.pdf")
+
+
+def plot41d(run_cnt=3):
+    # Setup figure
+    fig = plt.figure(figsize=(10, 5))
+    fig_ax = fig.gca()
+    fig_ax2 = fig_ax.twinx()
+
+    for t, c in product((1, 2), (1, 2)):
+
+        start_times = []
+        cpu_usage = []
+        qps = []
+        for i in range(run_cnt):
+            with open(f'c{c}_t{t}/memcached{i+1}_c{c}_t{t}', 'r') as input_file:
+                df = pd.read_csv(input_file, delim_whitespace=True, header=1)
+
+                start_times.append(df['ts_start'][0] / 1000)
+                df['ts_start'] = df['ts_start'].map(
+                    lambda x: x / 1000 - start_times[i])
+                df['ts_end'] = df['ts_end'].map(
+                    lambda x: x / 1000 - start_times[i])
+                qps.append(df)
+
+            with open(f'c{c}_t{t}/cpu{i+1}_c{c}_t{t}', 'r') as cpu_usage_file:
+                df = pd.read_csv(cpu_usage_file, header=None)
+                if c == 1:
+                    cpu_usage.append(pd.DataFrame(
+                        {'cpu': df[0], 'timestamp': df[4].map(lambda x: x - start_times[i])}))
+                else:
+                    cpu_usage.append(pd.DataFrame(
+                        {'cpu': df[0] + df[1], 'timestamp': df[4].map(lambda x: x - start_times[i])}))
+
+        mem_start = pd.DataFrame(
+            {f'run{i}': qps[i]['ts_start'] for i in range(run_cnt)})
+        mem_end = pd.DataFrame(
+            {f'run{i}': qps[i]['ts_end'] for i in range(run_cnt)})
+
+        mem_p95 = pd.DataFrame(
+            {f'run{i}': qps[i]['p95'] / 1000 for i in range(run_cnt)})
+
+        fig_ax.errorbar(
+            x=mem_start.mean(axis=1),
+            y=mem_p95.mean(axis=1),
+            xerr=mem_start.std(axis=1),
+            yerr=mem_p95.std(axis=1),
+            label=f'P95 T={t}, C={c}',
+            marker='o',
+            linestyle='-',
+            markersize=8,
+            markerfacecolor="none",
+            capsize=4
+        )
+
+        cpu_start = []
+        cpu_y = []
+        for start, end in zip(mem_start.mean(axis=1), mem_end.mean(axis=1)):
+            usage_sum = 0
+            cnt = 0
+            for i in range(run_cnt):
+                for i, measurement in cpu_usage[i].iterrows():
+                    if start <= measurement['timestamp'] <= end:
+                        usage_sum += measurement['cpu']
+                        cnt += 1
+
+            cpu_start.append(start)
+            cpu_y.append(usage_sum / cnt)
+
+        fig_ax2.errorbar(
+            x=cpu_start,
+            y=cpu_y,
+            # xerr=cpu_x.std(axis=1),
+            # yerr=cpu_y.std(axis=1),
+            label=f'CPU T={t}, C={c}',
+            marker='x',
+            linestyle='--',
+            markersize=6,
+            markerfacecolor="none",
+            capsize=4
+        )
+
+    # fig_ax.plot(
+    #     [0, 160],
+    #     [1.5, 1.5],
+    #     linestyle=':',
+    #     label='SLO'
+    # )
+
+    fig_ax.set_xlabel("Time", fontsize=16)
+    fig_ax.set_ylabel("95th %-tile response time (ms)", fontsize=16)
+    fig_ax.legend(loc='upper left', fontsize=12)
+    fig_ax.grid(True, color='lightgray', linestyle='--', linewidth=1)
+    fig_ax.tick_params(labelsize=12)
+
+    fig_ax.set_xlim(left=-5, right=165)
+    fig_ax.set_ylim(bottom=0, top=1.6)
+    # fig_ax.set_yticks(range(0, 11, 1))
+    # fig_ax.set_xticks(range(0, 80001, 10000), labelrotation=45)
+    # fig_ax.set_xticks(range(0, 80001, 5000), minor=True)
+
+    fig_ax2.legend(loc='lower right', fontsize=12)
+    fig_ax2.set_ylabel("CPU usage (%)", fontsize=16)
+    fig_ax2.set_ylim(bottom=0, top=160)
+    fig_ax2.set_yticks(range(0, 161, 20))
+
+    # Save plot
+    plt.tight_layout()
+    plt.savefig("plot41d.pdf")
+
+
+if __name__ == '__main__':
+    plot41d()
